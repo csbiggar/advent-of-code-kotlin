37c962d0411624d9487c0dcec74b39cff294ca78
Carolyn Biggar <carolyn.biggar@johnlewis.co.uk>
2018-12-31 03:27:16 +0000

    Day 5: Remove unused stuff

--- src/test/kotlin/day5/Day5.kt
+++ src/test/kotlin/day5/Day5.kt
@@ -26,39 +26,11 @@ class Day5 {
     }
 
     @Test
-    fun `should identify pair as same type`() {
-        assertThat(Pair('a', 'a').isSameType()).isTrue()
-        assertThat(Pair('a', 'A').isSameType()).isTrue()
-        assertThat(Pair('a', 'b').isSameType()).isFalse()
-    }
-
-    @Test
-    fun `should identify 2 chars as same type`() {
-        assertThat('a'.isSameTypeAs('a')).isTrue()
-        assertThat('a'.isSameTypeAs('A')).isTrue()
-        assertThat('a'.isSameTypeAs('b')).isFalse()
-    }
-
-    @Test
     fun `should identify two character string as same type`() {
         assertThat("aa".isSameType()).`as`("aa are the same type").isTrue()
         assertThat("aA".isSameType()).`as`("aA are the same type").isTrue()
         assertThat("ab".isSameType()).`as`("ab are different types").isFalse()
         assertThat("a".isSameType()).`as`("single characters are treated as the same type").isTrue()
-    }
-
-    @Test
-    fun `should identify pair as same polarity`() {
-        assertThat(Pair('a', 'a').isSamePolarity()).`as`("a,a are same polarity").isTrue()
-        assertThat(Pair('a', 'A').isSamePolarity()).`as`("a,A are different polarity").isFalse()
-        assertThat(Pair('a', 'b').isSamePolarity()).`as`("a,b are same polarity").isTrue()
-    }
-
-    @Test
-    fun `should identify 2 chars as same polarity`() {
-        assertThat('a'.isSamePolarityAs('a')).`as`("a,a are same polarity").isTrue()
-        assertThat('a'.isSamePolarityAs('A')).`as`("a,A are different polarity").isFalse()
-        assertThat('a'.isSamePolarityAs('b')).`as`("a,b are same polarity").isTrue()
     }
 
     @Test
@@ -95,11 +67,6 @@ class Day5 {
 
 }
 
-private fun Pair<Char, Char>.isSameType() = first.equals(second, ignoreCase = true)
 private fun String.isSameType() = this.length != 2 || this[0].equals(this[1], ignoreCase = true)
-private fun Char.isSameTypeAs(otherChar: Char) = this.equals(otherChar, ignoreCase = true)
-
-private fun Pair<Char, Char>.isSamePolarity() = first.isLowerCase() == second.isLowerCase()
 private fun String.isSamePolarity() = this.length != 2 || this[0].isLowerCase() == this[1].isLowerCase()
-private fun Char.isSamePolarityAs(otherChar: Char) = this.isLowerCase() == otherChar.isLowerCase()
 

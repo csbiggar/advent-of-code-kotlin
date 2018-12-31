53ad1fbfc6c116d10499e0254cafe6a9acb0f3f3
Carolyn Biggar <carolyn.biggar@johnlewis.co.uk>
2018-12-31 03:33:51 +0000

    Day 5: Find the length of the reduced real polymer (solve part 1)

--- src/test/kotlin/day5/Day5.kt
+++ src/test/kotlin/day5/Day5.kt
@@ -1,9 +1,27 @@
 package day5
 
 import org.assertj.core.api.Assertions.assertThat
+
 import org.junit.jupiter.api.Test
+import java.io.File
 
 class Day5 {
+
+    @Test
+    fun `should identify two character string as same type`() {
+        assertThat("aa".isSameType()).`as`("aa are the same type").isTrue()
+        assertThat("aA".isSameType()).`as`("aA are the same type").isTrue()
+        assertThat("ab".isSameType()).`as`("ab are different types").isFalse()
+        assertThat("a".isSameType()).`as`("single characters are treated as the same type").isTrue()
+    }
+
+    @Test
+    fun `should identify two character string as same polarity`() {
+        assertThat("aa".isSamePolarity()).`as`("a,a are same polarity").isTrue()
+        assertThat("aA".isSamePolarity()).`as`("a,A are different polarity").isFalse()
+        assertThat("ab".isSamePolarity()).`as`("a,b are same polarity").isTrue()
+        assertThat("a".isSamePolarity()).`as`("single characters are treated as same polarity").isTrue()
+    }
 
     @Test
     fun `should react to adjacent units of same type and differing polarity`() {
@@ -26,19 +44,15 @@ class Day5 {
     }
 
     @Test
-    fun `should identify two character string as same type`() {
-        assertThat("aa".isSameType()).`as`("aa are the same type").isTrue()
-        assertThat("aA".isSameType()).`as`("aA are the same type").isTrue()
-        assertThat("ab".isSameType()).`as`("ab are different types").isFalse()
-        assertThat("a".isSameType()).`as`("single characters are treated as the same type").isTrue()
+    fun `should reduce a more complex example`(){
+        assertThat(process("dabAcCaCBAcCcaDA")).isEqualTo("dabCBAcaDA")
     }
 
     @Test
-    fun `should identify two character string as same polarity`() {
-        assertThat("aa".isSamePolarity()).`as`("a,a are same polarity").isTrue()
-        assertThat("aA".isSamePolarity()).`as`("a,A are different polarity").isFalse()
-        assertThat("ab".isSamePolarity()).`as`("a,b are same polarity").isTrue()
-        assertThat("a".isSamePolarity()).`as`("single characters are treated as same polarity").isTrue()
+    fun `show me the answer to part 1 - number of units left after reduction`(){
+        val polymer = File(javaClass.getResource("advent-of-code-input-day-5.txt").toURI()).readText()
+        val reducedPolymer = process(polymer)
+        println("The size of the reduced polymer is ${reducedPolymer.length}")
     }
 
     private fun process(polymer: String): String {

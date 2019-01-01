9eb04c73eb39c00c5816f47e93e857cfa9e7e811
Carolyn Biggar <carolyn.biggar@johnlewis.co.uk>
2019-01-01 20:42:34 +0000

    Day 6: Find the nearest in list of coordinates to a given coordinate

--- src/test/kotlin/day6/Day6.kt
+++ src/test/kotlin/day6/Day6.kt
@@ -22,16 +22,16 @@ class Day6 {
             Coordinate(1, 2)
         )
 
-        assertThat(findGridSize(sameXDifferentY)).`as`("y coords out of order")
-            .isEqualTo(GridSize(Coordinate(1, 2), Coordinate(1, 10)))
+        assertThat(createGrid(sameXDifferentY)).`as`("y coords out of order")
+            .isEqualTo(Grid(Coordinate(1, 2), Coordinate(1, 10)))
 
         val sameYDifferentX = listOf(
             Coordinate(10, 1),
             Coordinate(2, 1)
         )
 
-        assertThat(findGridSize(sameYDifferentX)).`as`("x coords out of order")
-            .isEqualTo(GridSize(Coordinate(2, 1), Coordinate(10, 1)))
+        assertThat(createGrid(sameYDifferentX)).`as`("x coords out of order")
+            .isEqualTo(Grid(Coordinate(2, 1), Coordinate(10, 1)))
 
         val moreThanTwoCoords = listOf(
             Coordinate(8, 3),
@@ -42,20 +42,80 @@ class Day6 {
             Coordinate(8, 9)
         )
 
-        assertThat(findGridSize(moreThanTwoCoords)).`as`("many coordinates")
-            .isEqualTo(GridSize(Coordinate(1, 1), Coordinate(8, 9)))
+        assertThat(createGrid(moreThanTwoCoords)).`as`("many coordinates")
+            .isEqualTo(Grid(Coordinate(1, 1), Coordinate(8, 9)))
     }
 
-    private fun findGridSize(list: List<Coordinate>): GridSize {
+    @Test
+    fun `should find the nearest coordinate to another given coordinate`() {
+        val a = Coordinate(1, 1)
+        val b = Coordinate(1, 6)
+        val c = Coordinate(8, 3)
+        val d = Coordinate(3, 4)
+        val e = Coordinate(5, 5)
+        val f = Coordinate(8, 9)
+
+        val coords = listOf(a, b, c, d, e, f)
+
+        assertThat(coords.findNearestTo(Coordinate(1, 2))).isEqualTo(a)
+        assertThat(coords.findNearestTo(Coordinate(4, 4))).isEqualTo(d)
+
+        assertThat(coords.findNearestTo((Coordinate(5, 1)))).`as`("equidistant coords should map as")
+            .isEqualTo(Coordinate.NONE)
+    }
+
+//    @Test
+//    fun `should map grid entire grid to the nearest coord in list `() {
+//        val coords = listOf(
+//            Coordinate(1, 1),
+//            Coordinate(1, 4)
+//        )
+//
+//        listNearestCoordsInGrid(coords).containsAll()
+//
+//        createGrid(coords)
+//            .getAllCoordsInGrid()
+//
+//
+//    }
+//
+//    private fun listNearestCoordsInGrid(coords: List<Coordinate>) : List<Coordinate> {
+//
+//
+//    }
+
+    private fun createGrid(list: List<Coordinate>): Grid {
         val sortedCoordinates = list
             .sortedBy { it.y }
             .sortedBy { it.x }
-        return GridSize(sortedCoordinates[0], sortedCoordinates[list.size -1])
+        return Grid(sortedCoordinates[0], sortedCoordinates[list.size - 1])
     }
+}
 
+private fun List<Coordinate>.findNearestTo(otherCoordinate: Coordinate): Coordinate {
+    val coordinatesSortedByDistance = this
+        .map { it to it.distanceFrom(otherCoordinate) }
+        .sortedBy { (_, distanceToTarget) -> distanceToTarget }
+
+    val (closestCoordinate, closestDistance) = coordinatesSortedByDistance.first()
+
+    val equidistantCoordinatesExist = coordinatesSortedByDistance
+        .toMap()
+        .filter { (_, distance) -> distance == closestDistance }
+        .size > 1
+
+
+    return if (equidistantCoordinatesExist)
+        Coordinate.NONE
+    else
+        closestCoordinate
 }
 
 data class Coordinate(val x: Int, val y: Int) {
+    companion object {
+        val NONE = Coordinate(-1, -1)
+    }
+
     fun distanceFrom(anotherCoordinate: Coordinate): Int {
         val xDistance = (x - anotherCoordinate.x).absoluteValue
         val yDistance = (y - anotherCoordinate.y).absoluteValue
@@ -63,9 +123,17 @@ data class Coordinate(val x: Int, val y: Int) {
     }
 }
 
-data class GridSize(
+data class Grid(
     val topLeft: Coordinate,
     val bottomRight: Coordinate
-)
+) {
+    fun getAllCoordsInGrid() {
+        (topLeft.x..bottomRight.x).map { x ->
+            (topLeft.y..bottomRight.y).map { y ->
+                Coordinate(x, y)
+            }
+        }
+    }
+}
 
 
